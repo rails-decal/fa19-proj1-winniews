dcaeb11749886ce59cd9c26577a2540012a789fe
suwinnie <winniebsu@gmail.com>
2019-11-05 20:08:46 -0800

    worked up to part 4, barely started part5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     trainerless_pokemon = Pokemon.where(trainer: nil)
     @pokemon = trainerless_pokemon.sample
+    #current_trainer = Pokemon.where(trainer: current_trainer)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :trainers
+  
   root to: "home#index"
+  patch "capture", to: "pokemons#capture"
+  patch "damage", to: "pokemons#damage"
   resources :trainers
 end
